20249532e40f6b97e3ada327c9bed0f42e67f5b4
Parth Sharma <parthsharma@Parths-MacBook-Pro.local>
2022-01-24 16:42:04 +0530

    all prdts

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const app = express()
 const{products} = require('./data')
 app.get('/',(req,res)=>
 {
+    res.send('<h1>Home Page</h1><a href="/api/products">products</a>')
+})
+app.get('/api/products',(req,res)=>{
+    const newProducts = products.map((product)=>{
+        const {id,name,image} = product;
+        return{id,name,image}
+    })
     res.json(products)
 })
 app.listen(8000, () => {

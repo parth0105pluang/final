efc002315dcaa363539c598fe65aa3577497796d
Parth Sharma <parthsharma@Parths-MacBook-Pro.local>
2022-01-24 16:20:36 +0530

    read data.json

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
+const{products} = require('./data')
 app.get('/',(req,res)=>
 {
-    res.json([{name:'Parth'},{name:'Random'}])
+    res.json(products)
 })
 app.listen(8000, () => {
   console.log('server is listening on port 8000....')

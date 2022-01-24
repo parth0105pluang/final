1e63b9e49ffe48be7fd4e9118dbba23c6eed518b
Parth Sharma <parthsharma@Parths-MacBook-Pro.local>
2022-01-24 15:39:59 +0530

    copied index.html to public

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const app = express()
 // setup static and middleware
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
    res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
 
 })
-
+*/
 app.all('*', (req, res) => {
   res.status(404).send('resource not found')
 })

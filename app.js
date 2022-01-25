32e693bd151de211e5272a5ca9bcacfb23c0f4b3
Parth Sharma <parthsharma@Parths-MacBook-Pro.local>
2022-01-25 12:45:47 +0530

    post

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 const express = require('express')
 const app = express()
-const morgan = require('morgan')
-const logger = require('./logger')
-const authorize = require('./authorize')
-//req => middleware => res
-app.use(morgan('tiny'))
-
-app.get('/',(req,res)=>
+let { people } = require('./data')
+//static assets
+app.use(express.static('./methods-public'))
+app.use(express.urlencoded({extended: false}))
+app.get('/api/people',(req,res)=>
 {
-    res.send('Home')
+    res.status(200).json({success:true,data:people})
 })
-app.get('/about',(req,res)=>{
-    res.send('About')
+app.post('/login',(req,res)=>{
+    const{name} = req.body
+    if(name)
+    {
+        return res.status(200).send(`Welcome ${name}`)
+    }
+    res.status(401).send('POST')
 })
-app.get('/api/products',(req,res)=>
-{
-    res.send('Products')
-})
-app.listen(8000, () => {
-  console.log('Server is listening on port 8000....')
+app.listen(8080, () => {
+  console.log('Server is listening on port 8080....')
 })
